af83f73a5bc321cceb19f0c8bec9b2a820ba8bde
Viktor <SuvorovViktorA@gmail.com>
2020-09-23 14:39:46 +0300

    Matrix

---
+++ main.js
@@ -0,0 +1,31 @@
+// Matrix
+
+function matrix(N) {
+  let myMatrix = [],
+    rows = columns = 0,
+    steps = N - 1;
+
+  for (let i = 0; i < N; i++) myMatrix[i] = [];
+
+  for (let i = 1; i <= N ** 2; i++) {
+    myMatrix[rows][columns] = i;
+
+    if (rows === steps && columns === N - steps - 1) steps--;
+
+    if (
+      (columns >= rows && columns < steps) ||
+      (rows - 1 === columns && rows === N - steps - 1)
+    )
+      columns++;
+    // Move Down;
+    else if (columns <= rows && columns >= N - steps) columns--;
+    // Move Up
+    else if (rows <= columns && rows < steps) rows++;
+    // R move;
+    else if (rows >= columns && rows >= N - steps) rows--;
+    // L move;
+  }
+  return myMatrix;
+}
+
+console.log(matrix(8));
